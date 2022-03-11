a8c073f1888eff33cfacbfec44cd3d88725bef4e
Dennis Dang <dangggdennis@gmail.com>
2022-03-11 10:30:52 -0500

    add tokio runtime to children actors

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,63 @@
+#[cfg(feature = "tokio-runtime")]
+use bastion::prelude::*;
+
 use flow_sdk::prelude::*;
 use std::error::Error;
 
+#[cfg(feature = "tokio-runtime")]
+use tokio;
+
+#[cfg(feature = "tokio-runtime")]
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    println!("Hello, world!");
+    println!("Running flow nft gateway");
 
-    let _ = poll_chain().await?;
+    Bastion::init();
+
+    Bastion::start();
+
+    let supervisor = Bastion::supervisor(|sp| {
+        sp
+            // ...with a specific supervision strategy...
+            .with_strategy(SupervisionStrategy::OneForOne)
+            // ...and some supervised children groups...
+            .children(|children| {
+                // ...
+                children.with_exec(|ctx: BastionContext| {
+                    async move {
+                        // ...receiving and matching messages...
+                        msg! { ctx.recv().await?,
+                            // ref <name> are broadcasts.
+                            ref msg: &'static str => {
+                                println!("with msg {}", msg);
+                                let _ = poll_chain().await?
+                                println!("ran flow block {}", msg);
+                                ()
+                            };
+                            // <name> (without the ref keyword) are messages that have a unique recipient.
+                            msg: &'static str => {
+                                println!("with msg {}", msg)
+                            };
+                            // =!> refer to messages that can be replied to.
+                            msg: &'static str =!> {
+                                println!("with msg {}", msg)
+                            };
+                            // <name> that have the `_` type are catch alls
+                            _: _ => ();
+                        }
+
+                        // ...
+
+                        Ok(())
+                    }
+                })
+            })
+    })
+    .expect("Couldn't create the supervisor.");
+
+    let _ = supervisor.broadcast("poll");
+
+    Bastion::block_until_stopped();
 
     Ok(())
 }
